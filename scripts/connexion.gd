8917ec4912eae8da50df147df1c41f43cf1ce00e
Aaliyah <aaliyah.loubier@gmail.com>
2025-12-17 10:50:57 +0100

    gfkdjf

--- scripts/connexion.gd
+++ scripts/connexion.gd
@@ -1,14 +1,14 @@
 extends Control
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _process(_delta):
+	if Input.is_action_just_pressed("enter"):
+		$Informations.show()
 
 func _on_retour_pressed():
 	get_tree().change_scene_to_file("res://scenes/mainmenumain.tscn")
